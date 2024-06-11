265d1412da13aa114b794a58ad1c395308632b0d
kevin5112 <kevin5112@yahoo.com>
2024-06-11 15:49:07 -0700

    include function to update genres

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -78,8 +78,6 @@ exports.genre_create_post = [
         res.redirect(genre.url);
       }
     }
-
-    res.send('NOT IMPLEMENTED: Genre create POST');
   }),
 ];
 
@@ -123,10 +121,51 @@ exports.genre_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Genre update form on GET.
 exports.genre_update_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Genre update GET');
+  const genre = await Genre.findById(req.params.id).exec();
+
+  if (genre === null) {
+    const err = new Error('Genre not found');
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render('genre_form', {
+    title: 'Update Genre',
+    genre: genre,
+  });
 });
 
 // Handle Genre update on POST.
-exports.genre_update_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Genre update POST');
-});
+exports.genre_update_post = [
+  body('name', 'Genre name must contain atleast 3 characters')
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const genre = new Genre({
+      name: req.body.name,
+      _id: req.params.id, // required, or a new id will be assigned!
+    });
+
+    if (!errors.isEmpty()) {
+      const genre = await Genre.findById(req.params.id).exec();
+
+      res.render('genre_form', {
+        title: 'Update Genre',
+        genre: genre,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const updatedGenre = await Genre.findByIdAndUpdate(
+        req.params.id,
+        genre,
+        {}
+      );
+      res.redirect(updatedGenre.url);
+    }
+  }),
+];
